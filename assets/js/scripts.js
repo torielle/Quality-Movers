3aaec6a82469453214563799976aa7eb78c412dc
torielle <38533444+torielle@users.noreply.github.com>
2022-02-25 01:58:39 +0600

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -76,23 +76,23 @@
 		// Modal ////////////////////
 		
 		// Get the modal
-		var modal = document.getElementById('myModal');
+		//var modal = document.getElementById('myModal');
 
 		// Get the <span> element that closes the modal
-		var span = document.getElementsByClassName("close")[0];
+		//var span = document.getElementsByClassName("close")[0];
 
 		// When the user clicks on <span> (x), close the modal
-		span.onclick = function() {
-			modal.style.display = "none";
-		}
+		//span.onclick = function() {
+		//	modal.style.display = "none";
+		//}
 
 		// When the user clicks anywhere outside of the modal, close it
-		window.onclick = function(event) {
-			if (event.target == modal) {
-				modal.style.display = "none";
-			}
-		}
+		//window.onclick = function(event) {
+		//	if (event.target == modal) {
+		//		modal.style.display = "none";
+		//	}
+		//}
 		// When the user clicks submit, close modal
-		$('#subscribeButton').onclick = function() {
-			modal.style.display = "none";
-		}
+		//$('#subscribeButton').onclick = function() {
+		//	modal.style.display = "none";
+		//}
